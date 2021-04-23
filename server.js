e032e2562895dc9573b40f6ddd072dd1eff8b949
Marjorie Pomerleau <marjo.pomerleau@gmail.com>
2021-04-23 00:45:45 -0400

    heroku cors

--- server.js
+++ server.js
@@ -38,6 +38,16 @@ const corsOptions = {
   }
 }
 app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT ,DELETE");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 const mongoClient = require('mongodb').MongoClient;
 app.set('port', appPort);
 const server = http.createServer(app);
